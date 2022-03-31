1e645f788aa5dc5f9912bf7224d500edb0079557
frankguo1996 <58531736+frankguo1996@users.noreply.github.com>
2022-03-31 15:46:19 -0600

    Setting UI Edit modal done

--- client/src/components/Page Components/Settings/Settings.js
+++ client/src/components/Page Components/Settings/Settings.js
@@ -5,10 +5,14 @@ import ActionBar from "../../UI Components/ActionBar/ActionBar";
 import { Edit } from '@material-ui/icons';
 import Confirm from "../../UI Components/Modals/Confirm";
 import { useAuth } from "../../../context/UserAuthContext";
+import ChangePassword from "../../UI Components/Modals/ChangePassword";
+import ChangeUsername from "../../UI Components/Modals/ChangeUsername";
 
 
 function Settings(){
     const [confirmIsOpen, setConfirmIsOpen] = useState(false);
+    const [changePasswordIsOpen, setchangePasswordIsOpen] = useState(false);
+    const [changeUsernameIsOpen, setchangeUsernameIsOpen] = useState(false);
 
     const { user, changeUsername, changePassword, deleteAccount } = useAuth();
 
@@ -21,6 +25,24 @@ function Settings(){
         }
     }
 
+    const changePasswordHandler = async () => {
+        try {
+            await changePassword();
+            setchangePasswordIsOpen(false);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const changeUsernameHandler = async () => {
+        try {
+            await changeUsername();
+            setchangeUsernameIsOpen(false);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
     return(
         
         <div className="main-settings-div">
@@ -28,6 +50,8 @@ function Settings(){
             {confirmIsOpen && (<Confirm confirmHandler={deleteAccountHandler} cancelHandler={() => {setConfirmIsOpen(false)}} title="Are you sure you want to delete this account?">
                         You are about to delete an entire account, you will no longer be able to access listvine anymore.
                         </Confirm>)}
+            {changePasswordIsOpen && (<ChangePassword changePwHandler={changePasswordHandler} cancelHandler={() => {setchangePasswordIsOpen(false)}}></ChangePassword>)}
+            {changeUsernameIsOpen && (<ChangeUsername chanegUsHandler={changeUsernameHandler} cancelHandler={() => {setchangeUsernameIsOpen(false)}}></ChangeUsername>)}
             <div className="settings-div">
                 <div className="general-div">
                     <div className="header-section">
@@ -39,7 +63,7 @@ function Settings(){
                             <p className="username-p">Username:</p>
                             <p className="text-p">{user.displayName}</p>
                             <div className="edit-actions">
-                                <Edit fontSize="large"/>
+                                <Edit onClick={() => setchangeUsernameIsOpen(true)} fontSize="large"/>
                             </div>
                         </div>
                         <div className="email-section">
@@ -50,7 +74,7 @@ function Settings(){
                             <p className="password-p">Password: </p>
                             <p className="text-p">*********</p>
                             <div className="edit-actions">
-                                <Edit fontSize="large"/>
+                                <Edit onClick={() => setchangePasswordIsOpen(true)} fontSize="large"/>
                             </div>
                         </div>
                         
