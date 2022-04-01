877add7b53e76e9544d1beabbcb7c38f9823b4a6
Ishan Vyas <ishan.official01@gmail.com>
2022-03-31 18:56:56 -0600

    change username and password working

--- client/src/components/UI Components/Modals/ChangePassword.js
+++ client/src/components/UI Components/Modals/ChangePassword.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import Button from '../Button';
 import TextInput from '../TextInputs/TextInput';
 import { useAuth } from '../../../context/UserAuthContext';
@@ -10,6 +10,19 @@ const ChangePassword = (props) => {
     const [password, setPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        setError("");
+    },[password, newPassword, confirmNewPassword]);
+
+    const changePasswordPress = async () => {
+        try {
+            await props.changePwHandler(password, newPassword);
+        } catch (error) {
+            setError(error?.message);
+        }
+    }
 
     return(
         <div className="modalBackground">
@@ -21,10 +34,11 @@ const ChangePassword = (props) => {
                 <TextInput value={password} onChange={(e) => {setPassword(e.target.value)}} type='password'>Current Password:</TextInput>
                 <TextInput value={newPassword} onChange={(e) => {setNewPassword(e.target.value)}} type='password'>New Password:</TextInput>
                 <TextInput value={confirmNewPassword} onChange={(e) => {setConfirmNewPassword(e.target.value)}} type='password'>Confirm New Password:</TextInput>
+                {error && (<p style={{margin:'0', padding:'0', color:'red', marginBottom:'1%', fontSize:'large', textAlign:'center'}}>{error}</p>)}
                 </div>
                 <div className="modal-footer">
                     <Button onClick={props.cancelHandler} style={{backgroundColor: '#EDEFF2', color: 'black', marginRight:'5%'}}>Cancel</Button>
-                    <Button onClick={props.chanegPwHandler} style={{backgroundColor: '#EDEFF2', color: 'black', marginRight:'5%'}}>Change Password</Button>
+                    <Button onClick={changePasswordPress} style={{backgroundColor: '#EDEFF2', color: 'black', marginRight:'5%'}}>Change Password</Button>
                 </div>
             </div>
         </div>
